800e74de43047375e4c2647f719ea525f0147b94
cian murray <g00358011@atu.ie>
2023-04-25 15:36:40 +0100

    Created Submarine class and override to implement Floatable

--- src/main/java/ie/atu/Submarine.java
+++ src/main/java/ie/atu/Submarine.java
@@ -1,5 +1,16 @@
 package ie.atu;
 
-public class Submarine {
+public class Submarine implements Floatable{        //submarine class must adhere to Floatable contact
+    private String name;
+    private String year;
+
+    public Submarine(String name, String year) {
+        this.name = name;
+        this.year = year;
+    }
+
+    @Override
+    public String printDetails(){                  //Contract from Floatable interface
+    return "\nName: " + name + "\nYear: " + year;}
 
 }
